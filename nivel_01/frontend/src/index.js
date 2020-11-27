55d5e71ef855f18bad109535d48705ad3062010f
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-11-27 14:41:25 -0300

    app done

--- nivel_01/frontend/src/index.js
+++ nivel_01/frontend/src/index.js
@@ -1,5 +1,6 @@
-const soma = () => {
-  return a + b;
-}
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(1, 3));
+import App from './App';
+
+render(<App />, document.getElementById('app'));
