70a2bb84b33104bc6922afc4d092403f36e08f72
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-11-30 19:01:53 -0300

    eslint prettier config

--- nivel_02/backend/src/server.ts
+++ nivel_02/backend/src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 
+import routes from './routes'
+
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoStack11!' });
+})
+
 app.listen(3333, () => {
-  console.log('');
+  console.log('🚀 Server start on port 3333!');
 });
